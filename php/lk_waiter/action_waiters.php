bb80b09fb3fd663a5c5e849af4cd7e35230ce05a
Amankos Ansar <zugzwanggg0@gmail.com>
2024-08-13 22:12:54 +0500

    Updated new functionality

--- php/lk_waiter/action_waiters.php
+++ php/lk_waiter/action_waiters.php
@@ -7,13 +7,13 @@
   if(isset($_POST['manager_info'])){
     $id_cafe = FILTER($_POST['id_cafe']);
 
-    $query_s = $link->query("SELECT * FROM `manager` WHERE `id_cafe` = '$id_cafe'");
-    $manager = $query_s->fetch_assoc();
+    $query_s = $link->query("SELECT `manager`.telegram_id AS 'manager_telegram_id', `personal`.telegram_id AS 'telegram_id', `notice_orders`.id AS 'comment_id' FROM `notice-orders` JOIN `manager` ON `manager`.id_cafe = `notice_orders`.id_cafe JOIN `personal` ON `personal`.id_cafe = `notice_orders`.id_cafe WHERE `id_cafe` = '$id_cafe'");
+    $data = $query_s->fetch_assoc();
 
-    if($manager){
-        echo json_encode(['status' => 'success', 'data' => $manager]);
+    if($data){
+        echo json_encode(['status' => 'success', 'data' => $data]);
     } else {
-        echo json_encode(['status' => 'error', 'message' => 'Управляющий не найден']);
+        echo json_encode(['status' => 'error', 'message' => 'Ничего не найдено']);
     }
   }
 ?>
